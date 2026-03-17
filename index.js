8dea791b32b8282f15f75f626f26346917e2c1ec
AnirudhS3110 <anirudh23bcs19@iiitkottayam.ac.in>
2026-03-17 09:57:34 +0530

    initial

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Welcome</h1>
+    <p>Name: Anirudh S</p>
+    <p>Roll No: 2023BCS0019</p>
+    <p>Register No: YOUR REGISTER</p>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
